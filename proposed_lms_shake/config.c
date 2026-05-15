72cbd29d26e5ce8a710b6b0416666bfa93478290
mmiralle17 <99352147+mmiralle17@users.noreply.github.com>
2026-05-15 14:24:50 +0200

    Update comments and definitions in config.c

--- proposed_lms_shake/config.c
+++ proposed_lms_shake/config.c
@@ -1,16 +1,15 @@
-// config.h
 #ifndef CONFIG_H
 #define CONFIG_H
 
 #include <stdint.h>
 #include <stddef.h>
 
-// Parámetros para LMS con SHAKE-256/256
+// Parámetros para LMS con SHAKE-256
 #define HASH_LEN 32         // m = 32 bytes (256 bits)
-#define W_PARAM 8           // Winternitz w = 8 (Byte a byte, más fácil)
-#define H_TREE_HEIGHT 10    // Altura del árbol (puedes cambiarlo a 5 para pruebas rápidas)
+#define W_PARAM 8           // Winternitz w = 8 
+#define H_TREE_HEIGHT 10    // Altura del árbol 
 
-// Identificadores de tipo (solo informativo por ahora)
+// Identificadores de tipo
 #define LMOTS_SHA256_N32_W8 0x00000004
 
 #endif
